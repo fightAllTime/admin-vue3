4dbf7c5c7082e4210e4f299d2e3b4d7e455f5d2c
fightAllTime <1576960692@163.com>
2026-05-15 17:41:34 +0800

    2024-05-15 开发权限管理页面（2/10）：编辑table并实时渲染判断项

--- src/pages/permission/api/index.ts
+++ src/pages/permission/api/index.ts
@@ -1,12 +1,32 @@
+type PermissionType = "MENU" | "BUTTON" | "API";
+
 interface PermissionRow {
   permissionId: string;
   permissionName: string;
   permissionCode: string;
+  permissionType: PermissionType;
+
   parentId: string | null;
+
+  routePath?: string;
+  component?: string;
+  icon: string;
+
+  sortOrder?: number;
 }
 
 interface PermissionNode extends PermissionRow {
   children?: PermissionNode[];
 }
 
-export type { PermissionRow, PermissionNode };
+interface PermissionTypeMeta {
+  label: string;
+  type: string;
+}
+
+export type {
+  PermissionType,
+  PermissionRow,
+  PermissionNode,
+  PermissionTypeMeta,
+};
--- src/pages/permission/index.vue
+++ src/pages/permission/index.vue
@@ -1,59 +1,93 @@
 <template>
     <div>
         权限管理
+        <el-table :data="treeData" style="width: 100%" row-key="permissionId" border lazy
+            :tree-props="{ children: 'children' }">
+            <el-table-column prop="permissionName" label="permissionName" />
+            <el-table-column prop="permissionType" label="permissionType">
+                <template #default="{ row }">
+                    <el-tag :type="getPermissionMeta(row.permissionType).type">{{
+                        getPermissionMeta(row.permissionType).label }}</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column prop="permissionCode" label="权限标识" />
+        </el-table>
     </div>
 </template>
 
 <script setup lang="ts">
-import type { PermissionRow, PermissionNode } from './api';
+import type { PermissionType, PermissionRow, PermissionNode, PermissionTypeMeta } from './api';
 const mockData: PermissionRow[] = [
     {
-        permissionId: 'root',
-        permissionName: '初始节点',
-        permissionCode: 'code_0',
+        permissionId: 'sys_root',
+        permissionName: '权限管理',
+        permissionCode: 'sys:root',
+        permissionType: 'MENU',
         parentId: null,
+        routePath: '/permission/permissionMain',
+        component: 'PermissionMain.vue',
+        icon: 'Permission',
+        sortOrder: 0
     },
     {
-        permissionId: '1',
-        permissionName: '节点1',
-        permissionCode: 'code_0_1',
-        parentId: 'root'
+        permissionId: 'sys_user',
+        permissionName: '用户管理',
+        permissionCode: 'sys:user',
+        permissionType: 'MENU',
+        parentId: 'sys_root',
+        routePath: '/system/user',
+        component: 'SystemUser.vue',
+        icon: 'User',
+        sortOrder: 1
     },
     {
-        permissionId: '2',
-        permissionName: '节点2',
-        permissionCode: 'code_0_2',
-        parentId: 'root'
-    },
+        permissionId: 'sys_user_add',
+        permissionName: '用户管理新增',
+        permissionCode: 'sys:user:add',
+        permissionType: 'BUTTON',
+        parentId: 'sys_user',
+        icon: 'Add',
+        sortOrder: 2
+    }
 ]
 
 const buildTree = (list: PermissionRow[]): PermissionNode[] => {
-  const map = new Map<string, PermissionNode>()
-  const roots: PermissionNode[] = []
+    const map = new Map<string, PermissionNode>()
+    const roots: PermissionNode[] = []
 
-  // 1️⃣ 初始化所有节点
-  list.forEach(item => {
-    map.set(item.permissionId, {
-      ...item,
-      children: []
+    // 1️⃣ 初始化所有节点
+    list.forEach(item => {
+        map.set(item.permissionId, {
+            ...item,
+            children: []
+        })
     })
-  })
 
-  // 2️⃣ 挂载 children
-  list.forEach(item => {
-    const node = map.get(item.permissionId)!
-    if (item.parentId === null) {
-      roots.push(node)
-    } else {
-      map.get(item.parentId)?.children?.push(node)
-    }
-  })
+    // 2️⃣ 挂载 children
+    list.forEach(item => {
+        const node = map.get(item.permissionId)!
+        if (item.parentId === null) {
+            roots.push(node)
+        } else {
+            map.get(item.parentId)?.children?.push(node)
+        }
+    })
 
-  return roots
+    return roots
 }
 
-const TreeData = buildTree(mockData)
-console.log('TreeData', TreeData);
+const treeData = buildTree(mockData)
+
+const permissionTypeMap: Record<PermissionType, PermissionTypeMeta> = {
+    MENU: { label: '菜单', type: 'primary' },
+    BUTTON: { label: '按钮', type: 'success' },
+    API: { label: '接口', type: 'warning' },
+}
+
+const getPermissionMeta = (type: PermissionType) => {
+    return permissionTypeMap[type]
+}
+
 
 </script>
 
